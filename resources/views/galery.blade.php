5d6b92779e5530cc96edfb44841613cc20888a18
FrankSinatra1 <smarttv201404@gmail.com>
2019-01-31 09:55:19 +0300

    Update galery.blade.php

--- resources/views/galery.blade.php
+++ resources/views/galery.blade.php
@@ -1,9 +1,16 @@
 @extends("layouts.head")
 @section('content')
 
-<section class="wrapper-news">
-    <p class="titleSection">Фотогалерея</p>
-</section>
+		<section class="wrapper-news">
+		    <p class="titleSection">Фотогалерея</p>
+		    @foreach($gallery as $item)
+	            <div class="gallery-wrapper">
+	            	<h1>{{ $item->title }}</h1>
+	            	<img src="{{ asset('/storage/' . $item->photo) }}" alt="">
+	            </div>
+        	@endforeach
+		</section>
+	</div>
 
 
 @endsection
